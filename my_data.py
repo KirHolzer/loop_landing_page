f5215c0d5f1ef98035cab0fe2066dac114deffc4
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-07-09 01:53:41 +0300

    ADDED: class TestUrls

---
+++ my_data.py
@@ -0,0 +1,21 @@
+class TestData:
+    zxcv ='xcvx'
+
+
+class TestUrls:
+    main_url = 'https://loop-website-develop.stands.wilix.dev/'
+    tariff_page_url = 'https://loop-website-develop.stands.wilix.dev/pricing/'
+    support_page_url = 'https://loop-website-develop.stands.wilix.dev/support/'
+    download_apps_page_url = 'https://loop-website-develop.stands.wilix.dev/download/'
+    sign_up_page_url = 'https://loop-website-develop.stands.wilix.dev/sign-up/'
+    contact_sales_page_url = 'https://loop-website-develop.stands.wilix.dev/contact-sales/'
+    telegram_chat_support_page_url = 'https://t.me/loop_chat_support'
+    docs_for_users_url = 'https://docs.loop.ru/'
+    docs_for_developers_url = 'https://developers.loop.ru/'
+    docs_privacy_policy_url = 'http://loop-website-develop.stands.wilix.dev/privacy-policy/'
+    docs_terms_of_use_url = 'http://loop-website-develop.stands.wilix.dev/terms-of-use/'
+    docs_subscription_terms_url = 'http://loop-website-develop.stands.wilix.dev/subscription-terms/'
+
+
+
+
---
+++ tests/page_navigation.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+#from locators import TestLocators
+#from data import TestUrls
+#from data import TestConstructorNavigationText
+
+class PageNavigation:
+
+    def
