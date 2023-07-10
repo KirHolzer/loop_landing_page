a45a2ce4f8be3f3f93a999f4ba159f553340989d
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-07-10 18:49:29 +0300

    RESULT: on 10.07

--- locators.py
+++ locators.py
@@ -3,15 +3,38 @@ from selenium.webdriver.common.by import By
 class TestLocators:
 
 
-    LINK_TARIF_IN_HEADER = By.XPATH, ".//a[@href='/pricing/']"
-    LINK_SUPPORT_IN_HEADER = By.XPATH, ".//a[@href='/support/']"
-    LINK_DOWNLOAD_IN_HEADER = By.XPATH,  ".//a[@href='/download/']"
-
+    LINK_TARIF_IN_HEADER = By.XPATH, ".//header//a[@href='/pricing/']"
+    LINK_SUPPORT_IN_HEADER = By.XPATH, ".//header//a[@href='/support/']"
+    LINK_DOWNLOAD_IN_HEADER = By.XPATH,  ".//header//a[@href='/download/']"
+    BUTTON_CREATE_TEAM_IN_HEADER = By.XPATH, ".//header//button[text() = 'Создать команду']"
+    LOGO_LOOP_HEADER = By.XPATH, ".//header//img[@alt = 'logo']"
+    HEADING_ON_MAIN_PAGE = By.XPATH, ".//h1[text()='Единый мессенджер для вашей ']"
     HEADING_ON_SUPPORT_PAGE = By.XPATH,".//h2[text()='Написать нам']"
     HEADING_ON_DOWNLOAD_PAGE = By.XPATH, ".//h2[text() = 'Загрузить Loop для ']"
+    HEADING_ON_SIGN_UP_PAGE = By.XPATH, ".//h2[text() = 'Создать команду в Loop']"
+    HEADING_ON_CONTACT_SALES = By.XPATH, ".//h2[text() = 'Оставить заявку']"
     DOWNLOAD_FOR_ANDROID_ON_DOWNLOAD_PAGE = By.XPATH,  ".//button[text() = 'Скачать для Android']"
 
     HEADING_ON_TARIF_PAGE = By.XPATH, ".//h2[@class='py-8 text-center leading-tight']"
+
+    LINK_SIGN_UP_IN_FOOTER = By.XPATH, ".//footer//a[@href='/sign-up/']"
+    LINK_DOWNLOAD_IN_FOOTER = By.XPATH, ".//footer//a[@href='/download/']"
+    LINK_TARIF_IN_FOOTER = By.XPATH, ".//footer//a[@href='/pricing/']"
+    LINK_CONTACT_SALES_IN_FOOTER = By.XPATH, ".//footer//a[@href='/contact-sales/']"
+    LINK_SUPPORT_IN_FOOTER = By.XPATH, ".//footer//a[@href='/support/']"
+    lINK_LOOP_CHAT_SUPPORT_TELEG_IN_FOOTER = By.XPATH, ".//footer//a[@href='https://t.me/loop_chat_support']"
+    LINK_DOCS_FOR_USERS_IN_FOOTER = By.XPATH, ".//footer//a[@href = 'https://docs.loop.ru/']"
+    LINK_DOCS_FOR_DEVELOPERS_IN_FOOTER = By.XPATH, ".//footer//a[@href = 'https://developers.loop.ru/']"
+    LINK_DOCS_FOR_PRIVACY_POLICY_IN_FOOTER = By.XPATH, ".//footer//a[text() = 'Политика конфиденциальности']"
+
+    DOCS_ELEMENT_FOR_USERS = By.XPATH, ".//h1[text() = 'Добро пожаловать']"
+    DOCS_ELEMENT_FOR_DEVELOPERS = By.XPATH, ".//h1[text() = 'Добро пожаловать в документацию Loop для разработчиков']"
+    DOCS_ELEMENT_FOR_PRIVACY_POLICY = By.XPATH, ".//strong[0]"
+    # ".//a[text() = 'Руководство']"
+
+    LOCATOR_ON_TELEGRAM_PAGE = By.XPATH, ".//a[@class = 'tgme_head_brand']"
+
+
 
     EMAIL_INPUT_REGISTRATION = By.XPATH, ".//fieldset[2]//input"
     PASSWORD_INPUT_REGISTRATION = By.NAME, "Пароль"
--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -5,6 +5,7 @@ from my_data import TestUrls
 #from my_data import TestConstructorNavigationText
 
 class TestNavigation:
+
     def test_go_to_tarif_from_main_header(self, browser):
         browser.get(TestUrls.main_url)
         browser.find_element(*TestLocators.LINK_TARIF_IN_HEADER).click()
@@ -25,3 +26,85 @@ class TestNavigation:
         WebDriverWait(browser, 3).until(
             expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_DOWNLOAD_PAGE))
         assert browser.current_url == TestUrls.download_apps_page_url
+
+    def test_go_to_sign_up_from_main_header(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.BUTTON_CREATE_TEAM_IN_HEADER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_SIGN_UP_PAGE))
+        assert browser.current_url == TestUrls.sign_up_page_url
+
+    def test_go_to_main_from_sign_up_header(self, browser):
+        browser.get(TestUrls.sign_up_page_url)
+        browser.find_element(*TestLocators.LOGO_LOOP_HEADER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_MAIN_PAGE))
+        assert browser.current_url == TestUrls.main_url
+
+    def test_go_to_sign_up_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_SIGN_UP_IN_FOOTER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_SIGN_UP_PAGE))
+        assert browser.current_url == TestUrls.sign_up_page_url
+
+    def test_go_to_download_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_DOWNLOAD_IN_FOOTER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_DOWNLOAD_PAGE))
+        assert browser.current_url == TestUrls.download_apps_page_url
+
+    def test_go_to_tarif_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_TARIF_IN_FOOTER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_TARIF_PAGE))
+        assert browser.current_url == TestUrls.tariff_page_url
+
+    def test_go_to_contact_sales_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_CONTACT_SALES_IN_FOOTER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_CONTACT_SALES))
+        assert browser.current_url == TestUrls.contact_sales_page_url
+
+    def test_go_to_support_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_SUPPORT_IN_FOOTER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.HEADING_ON_SUPPORT_PAGE))
+        assert browser.current_url == TestUrls.support_page_url
+
+    def test_go_to_loop_chat_support_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.lINK_LOOP_CHAT_SUPPORT_TELEG_IN_FOOTER).click()
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.LOCATOR_ON_TELEGRAM_PAGE))
+        assert browser.current_url == TestUrls.telegram_chat_support_page_url
+
+    def test_go_to_docs_for_users_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_DOCS_FOR_USERS_IN_FOOTER).click()
+        browser.switch_to.window(browser.window_handles[-1])
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.DOCS_ELEMENT_FOR_USERS))
+        assert browser.current_url == TestUrls.docs_for_users_url
+
+    def test_go_to_docs_for_developers_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_DOCS_FOR_DEVELOPERS_IN_FOOTER).click()
+        browser.switch_to.window(browser.window_handles[-1])
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.DOCS_ELEMENT_FOR_DEVELOPERS))
+        assert browser.current_url == TestUrls.docs_for_developers_url
+
+    def test_go_to_docs_privocy_policy_from_main_footer(self, browser):
+        browser.get(TestUrls.main_url)
+        browser.find_element(*TestLocators.LINK_DOCS_FOR_PRIVACY_POLICY_IN_FOOTER).click()
+        browser.switch_to.window(browser.window_handles[-1])
+        WebDriverWait(browser, 3).until(
+            expected_conditions.visibility_of_element_located(TestLocators.DOCS_ELEMENT_FOR_PRIVACY_POLICY))
+        assert browser.current_url == TestUrls.docs_privacy_policy_url
+
+
